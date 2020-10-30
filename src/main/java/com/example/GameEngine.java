8d379077549c8a2d3199fa48360483b3b70828ba
Tomasz Zboralski <tomasz.zboralski@gmail.com>
2020-10-30 17:45:05 +0000

    Xs and Os added

--- src/main/java/com/example/GameEngine.java
+++ src/main/java/com/example/GameEngine.java
@@ -1,2 +1,101 @@
-package com.example;public class GameEngine {
+package com.example;
+
+import java.util.Arrays;
+
+public class GameEngine {
+
+    int[][] gameBoard;
+
+    public GameEngine(int[][] gameBoard) {
+        this.gameBoard = gameBoard;
+    }
+
+    public int checkLine(int[] line){
+        int player = 1;
+        int computer = 2;
+
+        boolean isPlayerWinner = Arrays.stream(line)
+                .allMatch(n -> n == player);
+
+        boolean isComputerWinner = Arrays.stream(line)
+                .allMatch(n -> n == computer);
+
+        if (isPlayerWinner){
+            return player;
+        } else if (isComputerWinner) {
+            return computer;
+        } else {
+            return 0;
+        }
+    }
+
+    public int[][] getGameBoard() {
+        return gameBoard;
+    }
+
+    public String getPosition(int row, int col){
+        String sign = "";
+        if (gameBoard[row][col] == 1){
+            sign = "O";
+        } else if (gameBoard[row][col] == 2){
+            sign = "X";
+        }
+
+        return sign;
+    }
+
+    public int[] getRow0() {
+        return gameBoard[0];
+    }
+
+    public int[] getRow1() {
+        return gameBoard[1];
+    }
+
+    public int[] getRow2() {
+        return gameBoard[2];
+    }
+
+    public int[] getCol0() {
+        return new int[]{gameBoard[0][0], gameBoard[1][0], gameBoard[2][0]};
+    }
+
+    public int[] getCol1() {
+        return new int[]{gameBoard[0][1], gameBoard[1][1], gameBoard[2][1]};
+    }
+
+    public int[] getCol2() {
+        return new int[]{gameBoard[0][2], gameBoard[1][2], gameBoard[2][2]};
+    }
+
+    public int[] getDiag0() {
+        return new int[]{gameBoard[0][0], gameBoard[1][1], gameBoard[2][2]};
+    }
+
+    public int[] getDiag1() {
+        return new int[]{gameBoard[0][2], gameBoard[1][1], gameBoard[2][0]};
+    }
+
+    public static void main(String[] args) {
+
+//        int[] row0 = gameBoard[0];
+//        int[] row1 = gameBoard[1];
+//        int[] row2 = gameBoard[2];
+//        int[] col0 = {gameBoard[0][0], gameBoard[1][0], gameBoard[2][0]};
+//        int[] col1 = {gameBoard[0][1], gameBoard[1][1], gameBoard[2][1]};
+//        int[] col2 = {gameBoard[0][2], gameBoard[1][2], gameBoard[2][2]};
+//        int[] diag0 = {gameBoard[0][0], gameBoard[1][1], gameBoard[2][2]};
+//        int[] diag1 = {gameBoard[0][2], gameBoard[1][1], gameBoard[2][0]};
+
+        int[][] gameBoard = {
+                {2, 1, 2},
+                {1, 1, 1},
+                {0, 0, 0}
+        };
+
+        GameEngine game = new GameEngine(gameBoard);
+        int x = game.checkLine(game.getRow1());
+
+        System.out.println(x);
+    }
 }
--- src/main/java/com/example/TicTacToe.java
+++ src/main/java/com/example/TicTacToe.java
@@ -23,12 +23,21 @@ public class TicTacToe extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        int[][] gameBoard = {
+                {1, 1, 2},
+                {2, 1, 1},
+                {2, 1, 1}
+        };
+
+        GameEngine game = new GameEngine(gameBoard);
+
+
         Group root = new Group();
-        Scene scene = new Scene(root, 800, 800, Color.DARKGRAY);
+        Scene scene = new Scene(root, 800, 800, Color.rgb(0, 100, 148));
 
         Text text = new Text();
         text.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 50));
-        text.setFill(Color.WHITE);
+        text.setFill(Color.rgb(130, 197, 233));
         text.setX(150);
         text.setY(80);
         text.setText("Tic-Tac-Toe Game");
@@ -40,34 +49,155 @@ public class TicTacToe extends Application {
         text.setEffect(reflection);
 
         Rectangle r = new Rectangle(100,100,600,600);
-        r.setFill(Color.GRAY);
+        r.setFill(Color.rgb(36, 123, 160));
 
-        // line \
-        Line lineA = new Line(150, 150, 650, 650);
-        lineA.setStrokeWidth(30);
-        // line /
-        Line lineB = new Line(150, 650, 650, 150);
-        lineB.setStrokeWidth(30);
-        // line |
-        Line lineC = new Line(400, 150, 400, 650);
-        lineC.setStrokeWidth(30);
-        //line --
-        Line lineD = new Line(150, 400, 650, 400);
-        lineD.setStrokeWidth(30);
 
         // #board
         Line line1 = new Line(300, 150, 300, 650);
+        line1.setStroke(Color.rgb(10, 71, 105));
         line1.setStrokeWidth(20);
         Line line2 = new Line(500, 150, 500, 650);
+        line2.setStroke(Color.rgb(10, 71, 105));
         line2.setStrokeWidth(20);
         Line line3 = new Line(150, 300, 650, 300);
+        line3.setStroke(Color.rgb(10, 71, 105));
         line3.setStrokeWidth(20);
         Line line4 = new Line(150, 500, 650, 500);
+        line4.setStroke(Color.rgb(10, 71, 105));
         line4.setStrokeWidth(20);
+
+        // line \
+        Line lineDiag0 = new Line(0, 0, 0, 0);
+        if (game.checkLine(game.getDiag0()) == 1 || game.checkLine(game.getDiag0()) == 2) {
+            lineDiag0 = new Line(150, 150, 650, 650);
+            lineDiag0.setStroke(Color.rgb(27, 152, 224));
+            lineDiag0.setStrokeWidth(30);
+        }
+
+        // line /
+        Line lineDiag1 = new Line(0, 0, 0, 0);
+        if (game.checkLine(game.getDiag1()) == 1 || game.checkLine(game.getDiag1()) == 2) {
+            lineDiag1 = new Line(150, 650, 650, 150);
+            lineDiag1.setStroke(Color.rgb(27, 152, 224));
+            lineDiag1.setStrokeWidth(30);
+        }
+
+        // line |
+        Line lineCol0 = new Line(0, 0, 0, 0);
+        if (game.checkLine(game.getCol0()) == 1 || game.checkLine(game.getCol0()) == 2) {
+            lineCol0 = new Line(200, 150, 200, 650);
+            lineCol0.setStroke(Color.rgb(27, 152, 224));
+            lineCol0.setStrokeWidth(30);
+        }
+
+        Line lineCol1 = new Line(0, 0, 0, 0);
+        if (game.checkLine(game.getCol1()) == 1 || game.checkLine(game.getCol1()) == 2) {
+            lineCol1 = new Line(400, 150, 400, 650);
+            lineCol1.setStroke(Color.rgb(27, 152, 224));
+            lineCol1.setStrokeWidth(30);
+        }
+
+        Line lineCol2 = new Line(0, 0, 0, 0);
+        if (game.checkLine(game.getCol2()) == 1 || game.checkLine(game.getCol2()) == 2) {
+            lineCol2 = new Line(600, 150, 600, 650);
+            lineCol2.setStroke(Color.rgb(27, 152, 224));
+            lineCol2.setStrokeWidth(30);
+        }
+
+        //row1 --
+        Line lineRow0 = new Line(0, 0, 0, 0);
+        if (game.checkLine(game.getRow0()) == 1 || game.checkLine(game.getRow0()) == 2) {
+            lineRow0 = new Line(150, 200, 650, 200);
+            lineRow0.setStroke(Color.rgb(27, 152, 224));
+            lineRow0.setStrokeWidth(30);
+        }
+
+        Line lineRow1 = new Line(0, 0, 0, 0);
+        if (game.checkLine(game.getRow1()) == 1 || game.checkLine(game.getRow1()) == 2) {
+            lineRow1 = new Line(150, 400, 650, 400);
+            lineRow1.setStroke(Color.rgb(27, 152, 224));
+            lineRow1.setStrokeWidth(30);
+        }
+
+        Line lineRow2 = new Line(0, 0, 0, 0);
+        if (game.checkLine(game.getRow2()) == 1 || game.checkLine(game.getRow2()) == 2) {
+            lineRow2 = new Line(150, 600, 650, 600);
+            lineRow2.setStroke(Color.rgb(27, 152, 224));
+            lineRow2.setStrokeWidth(30);
+        }
+
+        Text pos00 = new Text();
+        pos00.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, FontPosture.REGULAR, 150));
+        pos00.setFill(Color.rgb(130, 197, 233));
+        pos00.setX(150);
+        pos00.setY(250);
+        pos00.setText(game.getPosition(0,0));
+
+        Text pos01 = new Text();
+        pos01.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, FontPosture.REGULAR, 150));
+        pos01.setFill(Color.rgb(130, 197, 233));
+        pos01.setX(350);
+        pos01.setY(250);
+        pos01.setText(game.getPosition(0,1));
+
+        Text pos02 = new Text();
+        pos02.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, FontPosture.REGULAR, 150));
+        pos02.setFill(Color.rgb(130, 197, 233));
+        pos02.setX(525);
+        pos02.setY(250);
+        pos02.setText(game.getPosition(0,2));
+
+        Text pos10 = new Text();
+        pos10.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, FontPosture.REGULAR, 150));
+        pos10.setFill(Color.rgb(130, 197, 233));
+        pos10.setX(150);
+        pos10.setY(450);
+        pos10.setText(game.getPosition(1,0));
+
+        Text pos11 = new Text();
+        pos11.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, FontPosture.REGULAR, 150));
+        pos11.setFill(Color.rgb(130, 197, 233));
+        pos11.setX(350);
+        pos11.setY(450);
+        pos11.setText(game.getPosition(1,1));
+
+        Text pos12 = new Text();
+        pos12.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, FontPosture.REGULAR, 150));
+        pos12.setFill(Color.rgb(130, 197, 233));
+        pos12.setX(525);
+        pos12.setY(450);
+        pos12.setText(game.getPosition(1,2));
+
+        Text pos20 = new Text();
+        pos20.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, FontPosture.REGULAR, 150));
+        pos20.setFill(Color.rgb(130, 197, 233));
+        pos20.setX(150);
+        pos20.setY(650);
+        pos20.setText(game.getPosition(2,0));
+
+        Text pos21 = new Text();
+        pos21.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, FontPosture.REGULAR, 150));
+        pos21.setFill(Color.rgb(130, 197, 233));
+        pos21.setX(350);
+        pos21.setY(650);
+        pos21.setText(game.getPosition(2,1));
+
+        Text pos22 = new Text();
+        pos22.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, FontPosture.REGULAR, 150));
+        pos22.setFill(Color.rgb(130, 197, 233));
+        pos22.setX(525);
+        pos22.setY(650);
+        pos22.setText(game.getPosition(2,2));
 
 
         ObservableList<Node> list = root.getChildren();
-        list.addAll(r, text, lineA,  lineB, lineC, lineD, line1, line2, line3, line4);
+        list.addAll(r, text, line1, line2, line3, line4,
+                pos00, pos01, pos02,
+                pos10, pos11, pos12,
+                pos20, pos21, pos22,
+                lineDiag0, lineDiag1,
+                lineCol0, lineCol1, lineCol2,
+                lineRow0, lineRow1, lineRow2);
 
 
 
