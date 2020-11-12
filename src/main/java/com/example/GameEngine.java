80042ce73f6c2cac0fd29ed5978afeeee1f3055c
Tomasz Zboralski <tomasz.zboralski@gmail.com>
2020-11-12 22:13:05 +0000

    drawWinLine() simplified, computerMove() split into smaller methods, other improvments

--- src/main/java/com/example/GameEngine.java
+++ src/main/java/com/example/GameEngine.java
@@ -4,41 +4,36 @@ import java.util.*;
 import java.util.stream.IntStream;
 
 public class GameEngine {
-    int[][] gameBoard = {
-            {0, 0, 0},
-            {0, 0, 0},
-            {0, 0, 0}
-    };
 
-    int player;
-    int computer;
-    int playerScore = 0;
-    int computerScore = 0;
-    boolean nextMove = true;
+    private final char USER;
+    private final char COMPUTER;
+    private int userScore = 0;
+    private int computerScore = 0;
+    private boolean nextMove = true;
+    private char[][] gameBoard = new char[3][3];
 
-    public GameEngine(int player, int computer) {
-        this.player = player;
-        this.computer = computer;
+    public GameEngine(char user, char computer) {
+        this.USER = user;
+        this.COMPUTER = computer;
     }
 
-    public void isWinner(){
-        if (getWinnerLines(player).containsValue(true)) {
-            playerScore++;
+    public void checkWinner(){
+        if (getPlayerWinnerLines(USER).containsValue(true)) {
+            userScore++;
             nextMove = false;
-        }
-        if (getWinnerLines(computer).containsValue(true)){
+        } else if (getPlayerWinnerLines(COMPUTER).containsValue(true)){
             computerScore++;
             nextMove = false;
+        } else {
+            nextMove = true;
         }
     }
 
-    public Map<String, Boolean> getWinnerLines(int p){
-        return getWinnerLines(p, gameBoard);
+    public Map<String, Boolean> getPlayerWinnerLines(char player){
+        return getPlayerWinnerLines(player, gameBoard);
     }
 
-    public Map<String, Boolean> getWinnerLines(int p, int[][] board){
-
-
+    public Map<String, Boolean> getPlayerWinnerLines(char player, char[][] board){
         Map<String, Boolean> winnerLines = new HashMap<>();
 
         for(int i=0; i<3; i++){
@@ -46,115 +41,115 @@ public class GameEngine {
 
             boolean resultCol = IntStream.range(0,3)
                 .map(n -> board[n][x])
-                .allMatch(n -> n == p);
+                .allMatch(n -> n == player);
 
             winnerLines.put("lineCol" + i, resultCol);
 
-            boolean resultRow = Arrays.stream(board[x])
-                .allMatch(n -> n == p);
+            boolean resultRow = IntStream.range(0,3)
+                    .map(n -> board[x][n])
+                    .allMatch(n -> n == player);
 
             winnerLines.put("lineRow" + i, resultRow);
         }
 
         boolean lineDiag0 = IntStream.range(0,3)
                 .map(n -> board[n][n])
-                .allMatch(n -> n == p);
+                .allMatch(n -> n == player);
         winnerLines.put("lineDiag0", lineDiag0);
 
         boolean lineDiag1 = IntStream.range(0,3)
                 .boxed()
                 .sorted(Collections.reverseOrder())
                 .map(n -> board[n][2-n])
-                .allMatch(n -> n == p);
+                .allMatch(n -> n == player);
         winnerLines.put("lineDiag1", lineDiag1);
 
         return winnerLines;
     }
 
+    public Map<String, Boolean> getWinLines(){
+        Map<String,Boolean> userLine = getPlayerWinnerLines(USER);
+        Map<String,Boolean> computerLine = getPlayerWinnerLines(COMPUTER);
+
+        Map<String, Boolean> winnerLinesMerged = new HashMap<>(userLine);
+        computerLine.forEach((key, value) -> winnerLinesMerged
+                        .merge(key, value, (oldValue, newValue) -> oldValue || newValue));
+
+        return winnerLinesMerged;
+    }
+
     public void restartGame() {
-        gameBoard = new int[3][3];
+        gameBoard = new char[3][3];
         nextMove = true;
     }
 
-    public String markPosition(int row, int col){
-        String sign = "";
-        if (gameBoard[row][col] == 1){
-            sign = "O";
-        } else if (gameBoard[row][col] == 2){
-            sign = "X";
-        }
-        return sign;
-    }
+    public void smartComputerMove(){
+        char[][] temporaryBoard = Arrays.stream(gameBoard).map(char[]::clone).toArray(char[][]::new);
 
-    public void computerMove(){
-        boolean pickedField = false;
-        Random rand = new Random();
-
-        int[][] temporaryBoard = Arrays.stream(gameBoard).map(int[]::clone).toArray(int[][]::new);
-
-        // check if the move can win
         for (int i = 0; i < 3; i++) {
             for (int n = 0; n < 3; n++) {
-
-                if (temporaryBoard[i][n] == 0 && !pickedField) {
-                    temporaryBoard[i][n] = computer;
-                    if (getWinnerLines(computer, temporaryBoard).containsValue(true)){
-                        gameBoard[i][n] = computer;
-                        pickedField = true;
+                if (temporaryBoard[i][n] == 0 && nextMove) {
+                    temporaryBoard[i][n] = COMPUTER;
+                    if (getPlayerWinnerLines(COMPUTER, temporaryBoard).containsValue(true)){
+                        gameBoard[i][n] = COMPUTER;
+                        nextMove = false;
                     } else {
-                        temporaryBoard[i][n] = player;
+                        temporaryBoard[i][n] = USER;
                     }
-                    if (getWinnerLines(player, temporaryBoard).containsValue(true)) {
-                        gameBoard[i][n] = computer;
-                        pickedField = true;
+                    if (getPlayerWinnerLines(USER, temporaryBoard).containsValue(true)) {
+                        gameBoard[i][n] = COMPUTER;
+                        nextMove = false;
                     } else {
                         temporaryBoard[i][n] = 0;
                     }
                 }
             }
         }
+    }
 
-        // pick randomly if smart move impossible
-        while (!pickedField){
+    public void computerMove(){
+        smartComputerMove();
+        // pick a field randomly if smart move impossible
+        Random rand = new Random();
+        while (nextMove){
             int row = rand.nextInt(3);
             int col = rand.nextInt(3);
 
             if (gameBoard[row][col] == 0){
-                gameBoard[row][col] = 2;
-
-                pickedField = true;
+                gameBoard[row][col] = COMPUTER;
+                nextMove = false;
             }
-
-            // check if board is full
-            IntStream stream = Arrays.stream(gameBoard).flatMapToInt(Arrays::stream);
-            boolean isEmptyField = stream.anyMatch(x -> x == 0);
-            if (!isEmptyField){
-                pickedField = true;
+            if (!isEmptyFieldOnTheBoard()){
+                nextMove = false;
             }
         }
-        isWinner();
+        checkWinner();
     }
 
-    public void playerMove(int row, int col){
+    public boolean isEmptyFieldOnTheBoard() {
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                if(gameBoard[i][j] == 0) return true;
+            }
+        }
+        return false;
+    }
 
+    public void userMove(int row, int col){
         if (nextMove && gameBoard[row][col] == 0){
-            gameBoard[row][col]= 1;
-            isWinner();
+            gameBoard[row][col] = USER;
+            checkWinner();
             if (nextMove){
                 computerMove();
             }
         }
     }
 
-    public int getPlayer() {
-        return player;
-    }
-
-    public int getComputer() {
-        return computer;
+    public char[][] getGameBoard() {
+        return gameBoard;
     }
 
     public String getScoreBoard(){
-        return "Player [" + playerScore +"] : [" + computerScore + "] Computer ";
+        return "User [" + userScore +"] : [" + computerScore + "] Computer ";
     }
 }
--- src/main/java/com/example/TicTacToe.java
+++ src/main/java/com/example/TicTacToe.java
@@ -16,12 +16,11 @@ import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
 import java.util.Map;
 
 public class TicTacToe extends Application {
 
-    GameEngine game = new GameEngine(1, 2);
+    GameEngine game;
 
     Text title = new Text();
     Rectangle[] fields = new Rectangle[10];
@@ -34,6 +33,29 @@ public class TicTacToe extends Application {
     StackPane StackPaneCenter = new StackPane();
     Group winLines = new Group();
     HBox hBoxBottom = new HBox();
+
+    public void drawGame(){
+        createAllGrids();
+
+        title.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 50));
+        title.setFill(Color.rgb(130, 197, 233));
+        title.setText("Tic-Tac-Toe Game");
+
+        drawClickableFields();
+
+        buttonNewGame.setText("New Game");
+        buttonNewGame.setPrefSize(100, 50);
+        buttonNewGame.setOnAction((e) -> {
+            winLines.getChildren().clear();
+            game.restartGame();
+            drawMove();
+        });
+
+        scoreBoard.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 25));
+        scoreBoard.setFill(Color.rgb(130, 197, 233));
+        scoreBoard.setY(50);
+        scoreBoard.setText(game.getScoreBoard());
+    }
 
     public void createAllGrids(){
         mainGrid.setStyle("-fx-background-color: #006594");
@@ -57,27 +79,6 @@ public class TicTacToe extends Application {
         HBox.setMargin(scoreBoard,new Insets(20, 20, 40, 20));
     }
 
-    public void drawGame(){
-        title.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 50));
-        title.setFill(Color.rgb(130, 197, 233));
-        title.setText("Tic-Tac-Toe Game");
-
-        drawClickableFields();
-
-        buttonNewGame.setText("New Game");
-        buttonNewGame.setPrefSize(100, 50);
-        buttonNewGame.setOnAction((e) -> {
-            winLines.getChildren().clear();
-            game.restartGame();
-            drawMove();
-        });
-
-        scoreBoard.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 25));
-        scoreBoard.setFill(Color.rgb(130, 197, 233));
-        scoreBoard.setY(50);
-        scoreBoard.setText(game.getScoreBoard());
-    }
-
     public void drawClickableFields(){
         int x=0;
         for (int i = 0; i < 3; i++){
@@ -87,16 +88,16 @@ public class TicTacToe extends Application {
                 int a = i;
                 int b = n;
                 fields[x].setOnMouseClicked(t -> {
-                    game.playerMove(a,b);
+                    game.userMove(a,b);
                     drawMove();
-                    drawWinnerLine();
+                    drawWinLine();
                 });
                 gridPaneFields.add(fields[x],n,i);
 
                 moves[x] = new Text();
                 moves[x].setFont(Font.font("Comic Sans MS", FontWeight.BOLD, FontPosture.REGULAR, 150));
                 moves[x].setFill(Color.rgb(130, 197, 233));
-                moves[x].setText(game.markPosition(n,i));
+                moves[x].setText(positionToString(n,i));
                 gridPaneFields.add(moves[x],n,i);
                 GridPane.setHalignment(moves[x], HPos.CENTER);
 
@@ -105,79 +106,64 @@ public class TicTacToe extends Application {
         }
     }
 
+    public String positionToString(int row, int col){
+        String sign = Character.toString(game.getGameBoard()[row][col]);
+        return sign.toUpperCase();
+    }
+
     public void drawMove(){
         int x = 0;
         for (int i = 0; i < 3; i++) {
             for (int n = 0; n < 3; n++) {
-                moves[x].setText(game.markPosition(i, n));
+                moves[x].setText(positionToString(i, n));
                 x ++;
             }
         }
         scoreBoard.setText(game.getScoreBoard());
     }
 
-    public void drawWinnerLine(){
-        Map<String,Boolean> playerLine = game.getWinnerLines(game.getPlayer());
-        Map<String,Boolean> computerLine = game.getWinnerLines(game.getComputer());
+    public void drawWinLine(){
+        Map<String, Boolean> winLine = game.getWinLines();
 
-        Line lineDiag0 = new Line();
-        Line lineDiag1 = new Line();
-        Line lineCol0 = new Line();
-        Line lineCol1 = new Line();
-        Line lineCol2 = new Line();
-        Line lineRow0 = new Line();
-        Line lineRow1 = new Line();
-        Line lineRow2 = new Line();
+        Line winnerLine = new Line();
 
-        if (playerLine.get("lineDiag0") || computerLine.get("lineDiag0")){
-            lineDiag0 = new Line(30, 30, 500, 500);
-            lineDiag0.setStroke(Color.rgb(27, 152, 224));
-            lineDiag0.setStrokeWidth(30);
+        if (winLine.get("lineDiag0")){
+            winnerLine = new Line(30, 30, 500, 500);
         }
-        if (playerLine.get("lineDiag1") || computerLine.get("lineDiag1")) {
-            lineDiag1 = new Line(30, 500, 500, 30);
-            lineDiag1.setStroke(Color.rgb(27, 152, 224));
-            lineDiag1.setStrokeWidth(30);
+        if (winLine.get("lineDiag1")){
+            winnerLine = new Line(30, 500, 500, 30);
         }
-        if (playerLine.get("lineCol0") || computerLine.get("lineCol0")) {
-            lineCol0 = new Line(-450, 0, -450, 500);
-            lineCol0.setStroke(Color.rgb(27, 152, 224));
-            lineCol0.setStrokeWidth(30);
+        if (winLine.get("lineCol0")){
+            winnerLine = new Line(-450, 0, -450, 500);
         }
-        if (playerLine.get("lineCol1") || computerLine.get("lineCol1")) {
-            lineCol1 = new Line(0, 0, 0, 500);
-            lineCol1.setStroke(Color.rgb(27, 152, 224));
-            lineCol1.setStrokeWidth(30);
+        if (winLine.get("lineCol1")){
+            winnerLine = new Line(0, 0, 0, 500);
         }
-        if (playerLine.get("lineCol2") || computerLine.get("lineCol2")) {
-            lineCol2 = new Line(450, 0, 450, 500);
-            lineCol2.setStroke(Color.rgb(27, 152, 224));
-            lineCol2.setStrokeWidth(30);
+        if (winLine.get("lineCol2")){
+            winnerLine = new Line(450, 0, 450, 500);
         }
-        if (playerLine.get("lineRow0") || computerLine.get("lineRow0")) {
-            lineRow0 = new Line(-250, -400, 250, -400);
-            lineRow0.setStroke(Color.rgb(27, 152, 224));
-            lineRow0.setStrokeWidth(30);
+        if (winLine.get("lineRow0")){
+            winnerLine = new Line(-250, -400, 250, -400);
         }
-        if (playerLine.get("lineRow1") || computerLine.get("lineRow1")) {
-            lineRow1 = new Line(-250, 0, 250, 0);
-            lineRow1.setStroke(Color.rgb(27, 152, 224));
-            lineRow1.setStrokeWidth(30);
+        if (winLine.get("lineRow1")){
+            winnerLine = new Line(-250, 0, 250, 0);
         }
-        if (playerLine.get("lineRow2") || computerLine.get("lineRow2")) {
-            lineRow2 = new Line(-250, 500, 250, 500);
-            lineRow2.setStroke(Color.rgb(27, 152, 224));
-            lineRow2.setStrokeWidth(30);
+        if (winLine.get("lineRow2")){
+            winnerLine = new Line(-250, 500, 250, 500);
         }
 
-        winLines.getChildren().addAll(lineDiag0, lineDiag1,
-                lineCol0, lineCol1, lineCol2,
-                lineRow0, lineRow1, lineRow2);
+        if (winLine.containsValue(true)){
+            winnerLine.setStroke(Color.rgb(27, 152, 224));
+            winnerLine.setStrokeWidth(30);
+        }
+
+        winLines.getChildren().addAll(winnerLine);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        createAllGrids();
+    public void start(Stage primaryStage){
+        game = new GameEngine('o', 'x');
+
         drawGame();
 
         Scene scene = new Scene(mainGrid, 800, 800, Color.rgb(0, 100, 148));
@@ -185,6 +171,7 @@ public class TicTacToe extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+
     public static void main(String[] args) {
         launch(args);
     }
